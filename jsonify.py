41fa8b4e4ee9fea01b5e62d8bedd336671d65869
ltarabarova <ltarabarova@playwire.com>
2022-08-18 00:48:43 +0300

    tets

---
+++ jsonify.py
@@ -0,0 +1,14 @@
+import json
+import sys
+
+def toJson(data: str, delim=' | '):
+  return json.dumps(data.split(delim))
+
+if len(sys.argv) == 2:
+  print(toJson(sys.argv[1]))
+elif len(sys.argv) == 3:
+  print(toJson(sys.argv[1], sys.argv[2]))
+else:
+  sys.exit(1)
+
+sys.exit(0)
